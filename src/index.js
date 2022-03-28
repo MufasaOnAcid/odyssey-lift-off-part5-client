3c370da1bfca5ad62340d531eb9aa8599268d1f4
Mustafa Adam <mustafajeja@gmail.com>
2022-03-28 13:29:43 +0200

    Update server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-	uri: "https://mufasas-catronauts.herokuapp.com/",
+	uri: "https://mufasas-catronauts-server.herokuapp.com/",
 	cache: new InMemoryCache(),
 });
 
